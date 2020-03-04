49d820c55d09075cf39b69dd2a3a8d0e2ef1b320
Kasilus <its.an.omen.man@gmail.com>
2020-03-04 23:11:22 +0200

    vue-router config for upload - 2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    <ul class="top-level-menu">
+      <li><a class="active" href="#graph">Graph<i class="down menu-arrow"></i></a>
+        <ul class="second-level-menu">
+          <li><a href="#newgraph">New...<i class="right menu-arrow"></i></a>
+            <ul class="third-level-menu">
+              <li><a href="#manual">Manual</a></li>
+              <li><a href="#generate">Generate</a></li>
+            </ul>
+          </li>
+          <li><router-link :to="{name: 'graph/upload'}">Load from file</router-link></li>
+          <li><a href="#save">Save to file</a></li>
+        </ul>
+      </li>
+      <li><a href="#smp">SMP</a></li>
+      <li><a href="#modeling">Modeling<i class="down menu-arrow"></i></a>
+        <ul class="second-level-menu">
+          <li><a href="#cpuparams">CPU Parameters</a></li>
+          <li><a href="#gantt">Gantt Chart</a></li>
+          <li><a href="#modeling_stats">Statistics</a></li>
+        </ul>
+      </li>
+      <li><a href="#statistics">Statistics<i class="down menu-arrow"></i></a>
+        <ul class="second-level-menu">
+          <li><a href="#stats_parameters">Parameters</a></li>
+          <li><a href="#stats_results">Results</a></li>
+        </ul>
+      </li>
+      <li><a href="#help">Help</a></li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -31,30 +47,116 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .top-level-menu {
+    padding: 0;
+    height: 50px;
+    list-style-type: none;
+    background-color: #333;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .top-level-menu > li {
+    position: relative;
+    float: left;
+    width: 150px;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .top-level-menu > li:hover { background: #111; }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .top-level-menu > li a:hover:not(.active) {
+    background-color: #111;
+  }
 
-a {
-  color: #42b983;
-}
+  .top-level-menu li:hover > ul {
+    display: block;
+  }
+
+  .top-level-menu a {
+    display: block;
+    padding-left: 10px;
+    font: bold 14px Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    line-height: 50px;
+    color: white;
+  }
+
+  .second-level-menu {
+    display: none;
+    position: absolute;
+    top: 50px;
+    left: -40px;
+    width: 150px;
+    list-style-type: none;
+  }
+
+  .second-level-menu > li {
+    position: relative;
+    height: 50px;
+    background-color: #219A23;
+  }
+
+  .second-level-menu > li:hover { background: #111; }
+
+  .third-level-menu {
+    display: none;
+    position: absolute;
+    top: 0;
+    right: -150px;
+    width: 150px;
+    list-style-type: none;
+  }
+
+  .third-level-menu > li {
+    height: 50px;
+    background: #218923;
+  }
+
+  .third-level-menu > li:hover { background: #111; }
+
+  .active {
+    background-color: #21AB23;
+  }
+
+  i {
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+  }
+
+  .right {
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+  }
+
+  .down {
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+  }
+
+  .menu-arrow {
+    float: right;
+    margin-right: 14px;
+    margin-top: 18px;
+    border-color: white;
+  }
+
+  .active .menu-arrow {
+    border-color: black;
+  }
+
+  .second-level-menu > li .menu-arrow {
+    border-color: black;
+  }
+
+  .second-level-menu > li:hover .menu-arrow {
+    border-color: white;
+  }
+
+  .third-level-menu > li .menu-arrow {
+    border-color: black;
+  }
+
+  .third-level-menu > li:hover .menu-arrow {
+    border-color: white;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+import Hello from './components/Hello.vue'
+import Upload from './components/Upload.vue'
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Hello
+    },
+    {
+      path: '/graph/upload',
+      name: 'graph/upload',
+      component: Upload
+    }
+  ]
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
